6ae4fb250d5916ec3c0223bab1b6736eeba645fd
Carlton Joseph <carlton.joseph@gmail.com>
2019-05-27 17:42:13 -0700

    add cursor based pagination

--- src/components/Link/LinkList.js
+++ src/components/Link/LinkList.js
@@ -2,41 +2,81 @@ import React, { useContext, useEffect, useState } from "react";
 
 import FirebaseContext from "../../firebase/context";
 import LinkItem from "./LinkItem";
+import { LINKS_PER_PAGE } from "../../utils";
 
 function LinkList(props) {
   const { firebase } = useContext(FirebaseContext);
   const [links, linksSet] = useState([]);
+  const [cursor, cursorSet] = useState(null);
   const isNewPage = props.location.pathname.includes("new");
   const isTopPage = props.location.pathname.includes("top");
+  const page = Number(props.match.params.page);
 
   useEffect(() => {
     const unsubscribe = getLinks();
     return unsubscribe;
-  }, [isTopPage]);
+  }, [isTopPage, page]);
 
   function getLinks() {
+    const hasCursor = Boolean(cursor);
     if (isTopPage)
       return firebase.db
         .collection("links")
         .orderBy("voteCount", "desc")
+        .limit(LINKS_PER_PAGE)
         .onSnapshot(handleSnapshot);
-    return firebase.db
-      .collection("links")
-      .orderBy("created", "desc")
-      .onSnapshot(handleSnapshot);
+    if (page === 1)
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
+    if (hasCursor)
+      return firebase.db
+        .collection("links")
+        .orderBy("created", "desc")
+        .startAfter(cursor.created)
+        .limit(LINKS_PER_PAGE)
+        .onSnapshot(handleSnapshot);
   }
 
   function handleSnapshot(snapshot) {
     const links = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     linksSet(links);
+    const lastLink = links[links.length - 1];
+    cursorSet(lastLink);
   }
 
+  function visitPreviousPage() {
+    if (page > 1) props.history.push(`/new/${page - 1}`);
+  }
+  function visitNextPage() {
+    if (page <= links.length / LINKS_PER_PAGE)
+      props.history.push(`/new/${page + 1}`);
+  }
   if (!links || links.length === 0) return <div>No Links</div>;
+
+  const pageIndex = page ? (page - 1) * LINKS_PER_PAGE + 1 : 0;
   return (
     <div>
       {links.map((link, index) => (
-        <LinkItem key={link.id} {...link} showCount={true} index={index + 1} />
+        <LinkItem
+          key={link.id}
+          {...link}
+          showCount={true}
+          index={index + pageIndex}
+        />
       ))}
+      {isNewPage && (
+        <div className="pagination">
+          <div className="pointer mr2" onClick={visitPreviousPage}>
+            Previous
+          </div>
+          <div className="pointer" onClick={visitNextPage}>
+            Next
+          </div>
+        </div>
+      )}
     </div>
   );
 }
