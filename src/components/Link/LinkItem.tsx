2abbebc4fd07074c442eaab125d26d61784b3cdc
rasor <soren@rasor.dk>
2020-04-09 21:35:45 +0200

    LinkItem.tsx to ts

--- src/components/Link/LinkItem.tsx
+++ src/components/Link/LinkItem.tsx
@@ -1,11 +1,22 @@
 import React, { useContext } from "react";
 import { Link, withRouter } from "react-router-dom";
 import distanceInWordsToNow from "date-fns/distance_in_words_to_now";
-
 import { getDomain } from "../../utils";
 import FirebaseContext from "../../firebase/context";
 
-function LinkItem(props) {
+interface IProps {
+  description: string;
+  url: string;
+  showCount: boolean;
+  index: number;
+  postedBy: any;
+  created: string;
+  id: string;
+  comments: string;
+  history: any;
+  voteCount: string;
+}
+const LinkItem: React.FC<any> = (props) => {
   const { firebase, user } = useContext(FirebaseContext);
 
   const {
@@ -21,12 +32,12 @@ function LinkItem(props) {
     voteCount
   } = props;
 
-  function handleVote(e) {
+  function handleVote() {
     if (!user) return history.push("/login");
     const voteRef = firebase.db.collection("links").doc(id);
     voteRef.get().then(doc => {
       if (doc.exists) {
-        const previousVotes = doc.data().votes;
+        const previousVotes = doc.data()!.votes;
         const vote = { votedBy: { id: user.uid, name: user.displayName } };
         const updatedVotes = [...previousVotes, vote];
         const voteCount = updatedVotes.length;
@@ -35,7 +46,7 @@ function LinkItem(props) {
     });
   }
 
-  function handleDeleteLink(e) {
+  function handleDeleteLink() {
     const linkRef = firebase.db.collection("links").doc(id);
     linkRef
       .delete()
